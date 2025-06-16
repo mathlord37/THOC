43dd378d34e6ebc08fb14752b9998c31329b81a1
mathlord37 <raynew.21086@waitarahs.school.nz>
2025-06-17 10:26:08 +1200

    firstcommit

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+// This file contains the JavaScript code for the website. It adds interactivity and dynamic behavior to the webpage.
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to the Develop Outcome website!');
+    // Additional JavaScript code can be added here for interactivity
+});
